f3014a5896359e6700c28adb592d1d0d5b568251
cromicron <darionalis@gmail.com>
2023-04-05 17:18:55 +0200

    temporary irrelavant adjustments test_preprocessing

--- playground/test_preprocessing.py
+++ playground/test_preprocessing.py
@@ -9,21 +9,26 @@ from datetime import datetime
 
 file = "Frankfurt_LH_2301.h5"
 trajectories = [
-    Traffic.from_file(file, key = "LH_230115", parse_dates=["day","firstseen", "hour", "last_position","lastseen","timestamp"]),
-    Traffic.from_file(file, key = "LH_230116", parse_dates=["day","firstseen", "hour", "last_position","lastseen","timestamp"]),
-    Traffic.from_file(file, key = "LH_230117", parse_dates=["day","firstseen", "hour", "last_position","lastseen","timestamp"]),
-    Traffic.from_file(file, key = "LH_230122", parse_dates=["day","firstseen", "hour", "last_position","lastseen","timestamp"]),
-    Traffic.from_file(file, key = "LH_230123", parse_dates=["day","firstseen", "hour", "last_position","lastseen","timestamp"]),
-    Traffic.from_file(file, key = "LH_230124", parse_dates=["day","firstseen", "hour", "last_position","lastseen","timestamp"]),
+    Traffic.from_file(file, key = "LH_230115", parse_dates=[
+        "day","firstseen", "hour", "last_position","lastseen","timestamp"]),
+    Traffic.from_file(file, key = "LH_230116", parse_dates=[
+        "day","firstseen", "hour", "last_position","lastseen","timestamp"]),
+    Traffic.from_file(file, key = "LH_230117", parse_dates=[
+        "day","firstseen", "hour", "last_position","lastseen","timestamp"]),
+    Traffic.from_file(file, key = "LH_230122", parse_dates=[
+        "day","firstseen", "hour", "last_position","lastseen","timestamp"]),
+    Traffic.from_file(file, key = "LH_230123", parse_dates=[
+        "day","firstseen", "hour", "last_position","lastseen","timestamp"]),
+    Traffic.from_file(file, key = "LH_230124", parse_dates=[
+        "day","firstseen", "hour", "last_position","lastseen","timestamp"]),
 ]
 
 class TestPreprocessing(unittest.TestCase):
-
     
     def test_edge_flights(self):
         """
-        edge flights are those that have values at the end of the day or at the beginning of the day. This tests if all edge flights are captured 
-        and non edge flights are not captured
+        edge flights are those that have values at the end of the day or at the beginning of the day. This tests if
+        all edge flights are captured and non edge flights are not captured
         """
         
         time_str_early = "00:00:02"
@@ -45,9 +50,21 @@ class TestPreprocessing(unittest.TestCase):
             
     def test_get_combined_ids(self):
         """
-        for the edge flights, we need identifiers to find them in the other dataframes. The identifiers are concatenated icao24 and callsign.
-        This test uses mock examples and real examples
+        for the edge flights, we need identifiers to find them in the other dataframes. The identifiers are
+        concatenated icao24 and callsign.
         """
+        flights_middle = trajectories[2]
+        flights_before = trajectories[1]
+        flights_after = trajectories[3]
+        m_normal, m_early, m_late = get_edge_flights(flights_middle)
+        b_normal, b_early, b_late = get_edge_flights(flights_before)
+        a_normal, a_early, a_late = get_edge_flights(flights_after)
+
+        # create identifieres
+        ids_m_early = (flights_middle[m_early].data.icao24 + flights_middle[m_early].data.callsign).unique()
+        ids_b_late = (flights_before[b_late].data.icao24 + flights_before[b_late].data.callsign).unique()
+
+
         
         
         
