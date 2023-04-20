140e44d2ec012b533ee513ecc6f18e8912499b73
cromicron <darionalis@gmail.com>
2023-04-20 20:52:53 +0200

    new folder organisation and project structure to fascilitate fitting model

--- playground/test_train.py
+++ playground/test_train.py
@@ -8,7 +8,7 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import load_model
 from multiprocessing import Process, Queue
-from joblib import dump, load
+from joblib import dump, load as load_scaler
 import time
 
 
@@ -67,6 +67,9 @@ def load_data_batch(month_batch, data_queue):
 
 
 def load_data(queue, epochs, months, threads = 4):
+    if len(months)<threads:
+        print("warning fewer months than threads specified, reducing threads to number of months")
+        threads = len(months)
     month_batches = np.array_split(np.array(months), threads)
 
     for i in range(epochs):
@@ -77,8 +80,8 @@ def load_data(queue, epochs, months, threads = 4):
             process.start()
             processes.append(process)
         df_train = data_queue.get()
-        for _ in range(1,threads):
-            print(_)
+        for thread_nr in range(1,threads):
+            print(thread_nr)
             df_train = pd.concat([df_train, data_queue.get()])
         X = np.array(df_train[["distance", "altitude", "geoaltitude", "vertical_rate", "groundspeed"]])  # we only take the feature
         y = seconds_till_arrival(df_train)
@@ -88,31 +91,23 @@ def load_data(queue, epochs, months, threads = 4):
         queue.put((X, y))
 
 
-def fit_model(model, queue, file="model_small", batch_size=256):
-    X,y = queue.get()
-    while queue.empty():
-        model.fit(X,y, batch_size)
-        model.save(file)
-
-
 if __name__ == "__main__":
     print("Num GPUs Available: ", len(tf_config.list_physical_devices('GPU')))
     queue = Queue()
-    batch_size = 256
+    batch_size = 128
     file = "model_large"
 
-    scaler = StandardScaler()
-    model = build_sequential(lr=0.0001, input_dims=(5,),output_dims=1, layerSizes=(1024,512, 256),activation=None,loss="MSE")
-    #model = load_model("model_large", compile=False)
+    scaler = load_scaler("new_scaler.bin")
+    #model = build_sequential(lr=0.0001, input_dims=(5,),output_dims=1, layerSizes=(1024,512, 256),activation=None,loss="MSE")
+    model = load_model("model_large")
     months = ["01", "02", "03", "05", "06", "07", "08", "09", "10", "11"]
     load_data(queue, epochs=1, months=months, threads=5)
-    data_process = Process(target=load_data, args=(queue,10, months, 4))
+    data_process = Process(target=load_data, args=(queue,10, months, 3))
     data_process.start()
     while True:
        X, y = queue.get()
-       scaler.fit(X)
        X_scaled = scaler.transform(X)
-       dump(scaler, "new_scaler")
+
        while queue.empty():
            model.fit(X_scaled, y, batch_size=batch_size)
            model.save(file)
---
+++ train_model/train_model.py
@@ -0,0 +1,30 @@
+from tensorflow import keras, config as tf_config
+from tensorflow.keras.models import load_model
+from multiprocessing import Process, Queue
+from joblib import dump, load as load_scaler
+from data_loader import load_data
+import os
+
+if __name__ == "__main__":
+    data_files = []
+    for i in range(1, 13):
+        month = "0" + str(i) if i < 10 else str(i)
+        file = ".." + os.sep + "data" + os.sep + "Frankfurt_LH_22" + month + ".h5"
+        data_files.append(file)
+    print("Num GPUs Available: ", len(tf_config.list_physical_devices('GPU')))
+    queue = Queue()
+    batch_size = 128
+
+    scaler = load_scaler(".." + os.sep + "trained_models" + os.sep + "new_scaler.bin")
+    model = load_model(".." + os.sep + "trained_models" + os.sep + "model_large")
+
+    load_data(queue, epochs=1, flight_files=data_files, threads=5)
+    data_process = Process(target=load_data, args=(queue, 10, data_files, 3))
+    data_process.start()
+    while True:
+        X, y = queue.get()
+        X_scaled = scaler.transform(X)
+
+        while queue.empty():
+            model.fit(X_scaled, y, batch_size=batch_size)
+            model.save(file)
