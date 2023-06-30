2b19266b2e7238c76a8b7b3d58c8e3f0ae3f5188
saran <sarandeep1999@gmail.com>
2023-06-30 18:10:42 +0200

    add h3_preprocessing.py

---
+++ train_model/h3_preprocessing.py
@@ -0,0 +1,21 @@
+def h3_preprocess(data, res):
+    dfh3 = data
+    dfh3.rename(columns={'latitude':'lat', 'longitude':'lng'}, inplace=True)
+    print('Doing geo_to_h3')
+    dfh3 = dfh3.h3.geo_to_h3(res)
+    dfh3['h3index'] = dfh3.index  # Seperate column for h3 address
+    dfh3['timestamp'] = pd.to_datetime(dfh3['timestamp'])
+    dfh3 = dfh3[~(dfh3.h3index == '0')]  # These are due to NaN Latitude and Longitude
+    print('Doing h3_to_geo_boundary')
+    final = dfh3.h3.h3_to_geo_boundary()
+    final['h3_hour'] = final['timestamp'].dt.hour
+    print('dt.hour completed')
+    grouped = final.groupby(['h3index', 'h3_hour'])
+    print('Grouping according to hour and h3index completed')
+
+    final['hexbin_daily_density'] = grouped['h3index'].transform('count')
+    final['average_daily_speed'] = grouped['groundspeed'].transform('mean')
+    final['average_daily_altitude'] = grouped['altitude'].transform('mean')
+    final['average_eta'] = grouped['y'].transform('mean')
+    final.rename(columns={'lat': 'latitude', 'lng': 'longitude'}, inplace=True)
+    return final
