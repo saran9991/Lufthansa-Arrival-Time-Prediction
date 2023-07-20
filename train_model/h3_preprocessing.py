6d33aa136e83b4315cd2dd44d8092e6fdfe24123
saran <sarandeep1999@gmail.com>
2023-07-20 19:46:35 +0200

    update h3_preprocessing.py

--- train_model/h3_preprocessing.py
+++ train_model/h3_preprocessing.py
@@ -8,27 +8,23 @@ import h3pandas
 def h3_preprocess(data, res):
     dfh3 = data
     dfh3.rename(columns={'latitude': 'lat', 'longitude': 'lng'}, inplace=True)
-
-    print('Doing geo_to_h3')
     dfh3 = dfh3.h3.geo_to_h3(res)
-
     dfh3['h3index'] = dfh3.index  # Seperate column for h3 address
     dfh3['timestamp'] = pd.to_datetime(dfh3['timestamp'])
     dfh3 = dfh3[~(dfh3.h3index == '0')]  # These are due to NaN Latitude and Longitude
 
-    print('Doing h3_to_geo_boundary')
     final = dfh3.h3.h3_to_geo_boundary()
-
     final['h3_hour'] = final['timestamp'].dt.floor('H')
-    print('dt.hour completed')
-
     grouped = final.groupby(['h3index', 'h3_hour'])
-    print('Grouping according to hour and h3index completed')
 
     final['hexbin_hourly_density'] = grouped['flight_id'].transform('count')
     final['average_hourly_speed'] = grouped['groundspeed'].transform('mean')
     final['average_hourly_altitude'] = grouped['altitude'].transform('mean')
-    final['average_hourly_eta'] = grouped['y'].transform('mean')
+    #final['average_hourly_eta'] = grouped['y'].transform('mean')
 
-    final.rename(columns={'lat': 'latitude', 'lng': 'longitude'}, inplace=True)
+    final = final.rename(columns={'lat': 'latitude', 'lng': 'longitude'})
+    final.drop(columns = ['h3_hour', 'h3index', 'geometry'], inplace= True)
+    final.reset_index(inplace=True, drop=True)
+    print('H3 Features Added')
+
     return final
