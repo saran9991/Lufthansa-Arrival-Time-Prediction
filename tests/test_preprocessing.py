a624821fc234c055a1b61565e8afbdf9bb40c03e
cromicron <darionalis@gmail.com>
2023-09-29 13:42:34 +0200

    arrival time with noise remove test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import unittest
-from preprocessing.preprocessing import preprocess_traffic
+from src.preprocessing import preprocess_traffic
 
 class TestPreprocessing(unittest.TestCase):
     """Test all preprocessing functions """
@@ -39,7 +39,35 @@ class TestPreprocessing(unittest.TestCase):
         self.assertTrue(len(wrongly_not_extracted) == 0)
         self.assertTrue((df_extract.arrival_time == df_extract.arrival_time_true).all())
 
+    def test_arrival_time_with_noise_remove(self):
+        """
+        Same as test above, just with noise-remove function
+        """
+        cols_data = [
+            "day",
+            "firstseen",
+            "hour",
+            "last_position",
+            "lastseen",
+            "timestamp",
+            "arrival_time_true"
+        ]
+        print(os.getcwd())
+        file = "../data/testdata/test_data_arrival_time.csv"
+        df = pd.read_csv(file, parse_dates= cols_data)
+        df["identifier"] = df.firstseen.dt.date.astype(str) + df.callsign
 
+        no_landing = set(df.loc[df.arrival_time_true.isna(), "identifier"].unique())
+        landing = set(df.loc[df.arrival_time_true.notna(), "identifier"].unique())
+
+        df_extract = preprocess_traffic(df.copy(), remove_noise=True)
+        extracted_flights = set(df_extract.identifier.unique())
+        wrongly_extracted = extracted_flights.intersection(no_landing)
+        wrongly_not_extracted = landing - extracted_flights
+
+        self.assertTrue(len(wrongly_extracted) == 0)
+        self.assertTrue(len(wrongly_not_extracted) == 0)
+        self.assertTrue((df_extract.arrival_time == df_extract.arrival_time_true).all())
 
 
 
