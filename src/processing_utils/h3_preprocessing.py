e0cbfcd45509de9cdbfa51a46c7dcaa2db38f31e
cromicron <darionalis@gmail.com>
2023-10-04 10:23:50 +0200

    moved h3_preprocessing

---
+++ src/processing_utils/h3_preprocessing.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import h3
+import h3pandas
+
+
+# You have to restart kernel from what I noticed to run it again on the dataset for some reason if such a thing happens
+
+def h3_preprocess(data, res):
+    dfh3 = data
+    dfh3.rename(columns={'latitude': 'lat', 'longitude': 'lng'}, inplace=True)
+    dfh3 = dfh3.h3.geo_to_h3(res)
+    dfh3['h3index'] = dfh3.index  # Seperate column for h3 address
+    dfh3['timestamp'] = pd.to_datetime(dfh3['timestamp'])
+    dfh3 = dfh3[~(dfh3.h3index == '0')]  # These are due to NaN Latitude and Longitude
+
+    final = dfh3.h3.h3_to_geo_boundary()
+    final['h3_hour'] = final['timestamp'].dt.floor('H')
+    grouped = final.groupby(['h3index', 'h3_hour'])
+
+    final['hexbin_hourly_density'] = grouped['flight_id'].transform('count')
+    final['average_hourly_speed'] = grouped['groundspeed'].transform('mean')
+    final['average_hourly_altitude'] = grouped['altitude'].transform('mean')
+    #final['average_hourly_eta'] = grouped['y'].transform('mean')
+
+    final = final.rename(columns={'lat': 'latitude', 'lng': 'longitude'})
+    #final.drop(columns = ['h3_hour', 'h3index', 'geometry'], inplace= True)
+    final.reset_index(inplace=True, drop=True)
+    print('H3 Features Added')
+
+    return final
+
+def get_h3_index(data, res):
+    dfh3 = data
+    dfh3.rename(columns={'latitude': 'lat', 'longitude': 'lng'}, inplace=True)
+    dfh3 = dfh3.h3.geo_to_h3(res)
+    dfh3['h3index'] = dfh3.index  # Seperate column for h3 address
+    dfh3 = dfh3.rename(columns={'lat': 'latitude', 'lng': 'longitude'})
+    dfh3.reset_index(inplace=True, drop=True)
+    print('H3 Index Added')
+    return dfh3
+
+def weekday_column(traindata):
+    weekday_df = traindata.filter(regex='^weekday_')
+    traindata['weekday'] = weekday_df.idxmax(axis=1)
+    traindata['weekday'] = traindata['weekday'].str.replace('weekday_', '').astype(int)
+    return traindata
+
+
+def calculate_average_and_merge(traindata, testdata, group_cols, col, new_col_name):
+    mapping = traindata.groupby(group_cols)[col].mean()
+    mapping = mapping.reset_index().rename(columns={col: new_col_name})
+    return pd.merge(testdata, mapping, on=group_cols, how='left')
+
+
+def test_data_h3_average(traindata, testdata):
+    traindata['hour'] = traindata['timestamp'].dt.hour
+    testdata['hour'] = testdata['timestamp'].dt.hour
+
+    for group_cols, col, new_col_name in [
+        (['h3index', 'hour'], 'hexbin_hourly_density', 'average_hourly_hexbin_density'),
+        (['h3index', 'hour'], 'average_hourly_speed', 'average_hourly_avg_speed'),
+        (['h3index', 'hour'], 'average_hourly_altitude', 'average_hourly_avg_altitude'),
+    ]:
+        testdata = calculate_average_and_merge(traindata, testdata, group_cols, col, new_col_name)
+    testdata.rename(columns={'average_hourly_hexbin_density': 'hexbin_hourly_density', 'average_hourly_avg_speed': 'average_hourly_speed', 'average_hourly_avg_altitude':'average_hourly_altitude'}, inplace=True)
+    return testdata
+
