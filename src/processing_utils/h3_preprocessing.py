90fe3ca133af48263ec90e6caccc9cd45270d8dd
saran <sarandeep1999@gmail.com>
2023-10-11 19:16:49 +0200

    Add density metrics for aircraft data - past 10, 30, 60 minutes

--- src/processing_utils/h3_preprocessing.py
+++ src/processing_utils/h3_preprocessing.py
@@ -60,19 +60,57 @@ def plot_h3(df, save_html=False, file_name="map.html"):
     return m
 
 def add_density(df):
+    '''
+    This function adds density metrics to the given DataFrame based on the number of unique aircraft
+    in the same h3index within the past 10, 30, and 60 minutes.
+
+    Parameters:
+    - df: The input DataFrame containing aircraft data with timestamp, h3index, and flight_id columns.
+
+    Returns:
+    - The DataFrame with added density columns for the past 10, 30, and 60 minutes.
+    '''
+
     df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['density_30_minutes_past'] = 0
-    for index, row in df.iterrows():
-        current_time = row['timestamp'] #4:30PM
-        current_h3index = row['h3index'] # index 1
-        current_flight_id = row['flight_id']
-        past_30_minutes_records = df[
-            (df['timestamp'] < current_time) &
-            (df['timestamp'] >= current_time - timedelta(minutes=30)) &
-            (df['h3index'] == current_h3index) &
-            (df['flight_id'] != current_flight_id)
-            ]
-        df.at[index, 'density_30_minutes_past'] = past_30_minutes_records['flight_id'].nunique()
+    df = df.sort_values('timestamp').reset_index(drop=True)
+
+    density_10_minutes_past = []
+    density_30_minutes_past = []
+    density_60_minutes_past = []
+
+    h3index_flight_ids = {}
+
+    for i in tqdm(range(len(df)), desc="Processing", unit="rows"):
+        current_time = df.loc[i, 'timestamp']
+        current_h3index = df.loc[i, 'h3index']
+        current_flight_id = df.loc[i, 'flight_id']
+
+        if current_h3index not in h3index_flight_ids:
+            h3index_flight_ids[current_h3index] = {}
+
+        for flight_id, timestamp in list(h3index_flight_ids[current_h3index].items()):
+            if timestamp < current_time - timedelta(minutes=60):
+                del h3index_flight_ids[current_h3index][flight_id]
+
+        h3index_flight_ids[current_h3index][current_flight_id] = current_time
+
+        count_10 = len([flight_id for flight_id, timestamp in h3index_flight_ids[current_h3index].items()
+                        if flight_id != current_flight_id and timestamp >= current_time - timedelta(minutes=10)])
+
+        count_30 = len([flight_id for flight_id, timestamp in h3index_flight_ids[current_h3index].items()
+                        if flight_id != current_flight_id and timestamp >= current_time - timedelta(minutes=30)])
+
+        count_60 = len([flight_id for flight_id in h3index_flight_ids[current_h3index]
+                        if flight_id != current_flight_id])
+
+        density_10_minutes_past.append(count_10)
+        density_30_minutes_past.append(count_30)
+        density_60_minutes_past.append(count_60)
+
+    df['density_10_minutes_past'] = density_10_minutes_past
+    df['density_30_minutes_past'] = density_30_minutes_past
+    df['density_60_minutes_past'] = density_60_minutes_past
+
     return df
 
 def weekday_column(traindata):
