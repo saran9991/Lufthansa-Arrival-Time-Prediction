6c3b1ec11f8de0b86025f31c5d2200ef382c42ce
saran <sarandeep1999@gmail.com>
2023-10-16 01:23:19 +0200

    added bayesian optimization for XGBoost

--- src/models/xgb_model.py
+++ src/models/xgb_model.py
@@ -2,33 +2,18 @@ from sklearn.metrics import mean_absolute_error, r2_score
 import xgboost as xgb
 from joblib import load, dump
 import pandas as pd
-from src.processing_utils.preprocessing import generate_aux_columns
-from src.processing_utils.h3_preprocessing import get_h3_index, add_density
 
 class XGBModel:
-    def __init__(self, features, model_file=None, **kwargs):
-        self.feature_columns = features
+    def __init__(self, model_file=None, **kwargs):
         self.model = xgb.XGBRegressor(**kwargs) if model_file is None else load(model_file)
 
     def feature_importance_(self):
         return self.model.feature_importances_
 
-    def preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
-        data = data.copy()
-        data = generate_aux_columns(data)
-        data = get_h3_index(data, 4)
-        data = add_density(data)
-        data = data[self.feature_columns]
-        return data
-
-    def fit(self, X_train: pd.DataFrame, y_train: pd.Series, preprocess=True) -> None:
-        if(preprocess):
-            X_train = self.preprocess(X_train)
+    def fit(self, X_train: pd.DataFrame, y_train: pd.Series) -> None:
         self.model.fit(X_train, y_train)
 
-    def evaluate(self, X_test: pd.DataFrame, y_test: pd.Series, preprocess = True) -> (float, float):
-        if(preprocess):
-            X_test = self.preprocess(X_test)
+    def evaluate(self, X_test: pd.DataFrame, y_test: pd.Series) -> (float, float):
         y_pred = self.predict(X_test)
         mae = mean_absolute_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
--- src/train/train_xgboost.py
+++ src/train/train_xgboost.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from src.models.xgb_model import XGBModel
 from src.processing_utils.preprocessing import seconds_till_arrival
+from bayes_opt import BayesianOptimization
+from src.processing_utils.preprocessing import generate_aux_columns
+from src.processing_utils.h3_preprocessing import get_h3_index, add_density
 
 # Paths
 PATH_TRAIN_DATA = '../../data/train_data/training_data.csv'
@@ -20,23 +23,78 @@ N_ESTIMATORS = 500
 MAX_DEPTH = 6
 TREE_METHOD ="gpu_hist"
 
+
+def preprocess_xgb(data: pd.DataFrame, feature_columns) -> pd.DataFrame:
+    data = data.copy()
+    data = generate_aux_columns(data)
+    data = get_h3_index(data, 4)
+    data = add_density(data)
+    data = data[feature_columns]
+    return data
+
+
+def tune_hyperparameters(X_train, y_train, X_test, y_test, tree_method):
+    def optimize_xgb(n_estimators, max_depth, learning_rate, gamma, subsample, colsample_bytree):
+        n_estimators = int(n_estimators)
+        max_depth = int(max_depth)
+
+        xgb_model = XGBModel(n_estimators=n_estimators,
+                             max_depth=max_depth,
+                             learning_rate=learning_rate,
+                             gamma=gamma,
+                             subsample=subsample,
+                             colsample_bytree=colsample_bytree,
+                             tree_method=tree_method)
+        xgb_model.fit(X_train, y_train)
+
+        mae, _ = xgb_model.evaluate(X_test, y_test)
+        return -mae
+
+    bounds = {
+        'n_estimators': (100, 2000),
+        'max_depth': (3, 15),
+        'learning_rate': (0.01, 0.5),
+        'gamma': (0, 5),
+        'subsample': (0.5, 1),
+        'colsample_bytree': (0.5, 1)
+    }
+
+    optimizer = BayesianOptimization(f=optimize_xgb, pbounds=bounds, random_state=1)
+    optimizer.maximize(init_points=10,
+                       n_iter=100)
+
+    best_params = optimizer.max['params']
+    best_params['n_estimators'] = int(best_params['n_estimators'])
+    best_params['max_depth'] = int(best_params['max_depth'])
+    best_mae = -optimizer.max['target']
+
+    print(f"Best parameters found: {best_params} with MAE: {best_mae}")
+
+    return best_params
+
+
 if __name__ == "__main__":
 
     print('Reading Training DataFrame...')
     X_train = pd.read_csv(PATH_TRAIN_DATA, parse_dates=["arrival_time", "timestamp"]).reset_index(drop=True)
-    X_train['flight_id'] = X_train.arrival_time
     y_train = seconds_till_arrival(X_train)
+    X_train['flight_id'] = X_train.arrival_time
+    X_train = preprocess_xgb(X_train, FEATURES)
 
     print('Reading Testing DataFrame...')
     X_test = pd.read_csv(PATH_TEST_DATA, parse_dates=["arrival_time", "timestamp"]).reset_index(drop=True)
     y_test = seconds_till_arrival(X_test)
+    X_test['flight_id'] = X_test.arrival_time
+    X_test = preprocess_xgb(X_test, FEATURES)
 
-    xgb_model = XGBModel(features=FEATURES, n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH, tree_method = TREE_METHOD)
+    xgb_model = XGBModel(n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH, tree_method = TREE_METHOD)
     xgb_model.fit(X_train, y_train)
 
-    mae, r2 = xgb_model.evaluate(X_test, y_test, preprocess=True)
+    mae, r2 = xgb_model.evaluate(X_test, y_test)
     print(f"Mean Absolute Error: {mae}")
     print(f"R^2 Score: {r2}")
+
+    best_params = tune_hyperparameters(X_train, y_train, X_test, y_test, TREE_METHOD)
 
     importances = xgb_model.feature_importance_()
     df = pd.DataFrame({'features': FEATURES, 'importances': importances})
