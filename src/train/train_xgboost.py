4d5828bc1719f549bfe14ea9ba6eb22f8e1f5068
saran <sarandeep1999@gmail.com>
2024-03-08 15:42:43 +0100

    updated train_xgboost.py

--- src/train/train_xgboost.py
+++ src/train/train_xgboost.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 from src.models.xgb_model import XGBModel
 from src.processing_utils.preprocessing import seconds_till_arrival
 from bayes_opt import BayesianOptimization
 from src.processing_utils.preprocessing import generate_aux_columns
 from src.processing_utils.h3_preprocessing import get_h3_index, add_density
 import os
+import json
 
 # Paths
 PATH_TRAIN_DATA = '../../data/train_data/training_data.csv'
@@ -36,7 +38,13 @@ def preprocess_xgb(data: pd.DataFrame, feature_columns) -> pd.DataFrame:
     return data
 
 
-def tune_hyperparameters(X_train, y_train, X_test, y_test, tree_method):
+def tune_hyperparameters(X_train, y_train, tree_method):
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+    X_train.reset_index(inplace=True, drop=True)
+    X_val.reset_index(inplace=True, drop=True)
+    y_train.reset_index(inplace=True, drop=True)
+    y_val.reset_index(inplace=True, drop=True)
     def optimize_xgb(n_estimators, max_depth, learning_rate, gamma, subsample, colsample_bytree, reg_alpha):
         n_estimators = int(n_estimators)
         max_depth = int(max_depth)
@@ -52,7 +60,7 @@ def tune_hyperparameters(X_train, y_train, X_test, y_test, tree_method):
         xgb_model.fit(X_train, y_train)
 
         # Evaluate the model on the test set
-        mae, mae_relative, _ = xgb_model.evaluate(X_test, y_test)
+        mae, mae_relative, _ = xgb_model.evaluate(X_val, y_val)
         return -mae  # Objective is to minimize MAE, hence return negative MAE
 
     # Search space
@@ -68,7 +76,7 @@ def tune_hyperparameters(X_train, y_train, X_test, y_test, tree_method):
 
     # Configure the optimizer with the function and parameter bounds
     optimizer = BayesianOptimization(f=optimize_xgb, pbounds=bounds, random_state=1)
-    optimizer.maximize(init_points=100, n_iter=1000)
+    optimizer.maximize(init_points=2, n_iter=5)
 
     # Extract the best parameters and their corresponding MAE
     best_params = optimizer.max['params']
@@ -84,29 +92,43 @@ def tune_hyperparameters(X_train, y_train, X_test, y_test, tree_method):
 if __name__ == "__main__":
 
     print('Reading Training DataFrame...')
-    X_train = pd.read_csv(PATH_TRAIN_DATA, parse_dates=["arrival_time", "timestamp"]).reset_index(drop=True)
-    y_train = seconds_till_arrival(X_train) / (X_train['distance'] + EPSILON)
-    X_train['flight_id'] = X_train.arrival_time
-    X_train = preprocess_xgb(X_train, FEATURES)
+    X_train = pd.read_csv('../../data/pre_processed/train_data_xgb.csv')
+    y_train = pd.read_csv('../../data/pre_processed/y_train_data_xgb.csv').squeeze()
+    #X_train['flight_id'] = X_train.arrival_time
+    #X_train = preprocess_xgb(X_train, FEATURES)
 
     print('Reading Testing DataFrame...')
-    X_test = pd.read_csv(PATH_TEST_DATA, parse_dates=["arrival_time", "timestamp"]).reset_index(drop=True)
-    y_test = seconds_till_arrival(X_test) / (X_test['distance'] + EPSILON)
-    X_test['flight_id'] = X_test.arrival_time
-    X_test = preprocess_xgb(X_test, FEATURES)
+    X_test = pd.read_csv('../../data/pre_processed/X_test_data_xgb.csv')
+    y_test = pd.read_csv('../../data/pre_processed/y_test_data_xgb.csv').squeeze()
+    #X_test['flight_id'] = X_test.arrival_time
+    #X_test = preprocess_xgb(X_test, FEATURES)
 
-    xgb_model = XGBModel(n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH, tree_method = TREE_METHOD)
+    '''
+        xgb_model = XGBModel(n_estimators=N_ESTIMATORS, max_depth=MAX_DEPTH, tree_method = TREE_METHOD)
+        xgb_model.fit(X_train, y_train)
+
+        mae, mae_relative, r2 = xgb_model.evaluate(X_test, y_test)
+        print(f"Mean Absolute Error: {mae}")
+        print(f"Relative Mean Absolute Error: {mae_relative}")
+        print(f"R^2 Score: {r2}")
+    '''
+
+    best_params = tune_hyperparameters(X_train, y_train, TREE_METHOD) #Bayesian Optimization
+    best_params_path = os.path.join('..', '..', 'trained_models', 'best_params.json')
+    with open(best_params_path, 'w') as f:
+        json.dump(best_params, f)
+    print(f"Best parameters saved to {best_params_path}")
+
+    print('Training XGB on Optimal Paramter Configuration')
+    xgb_model = XGBModel(**best_params, tree_method = TREE_METHOD)
     xgb_model.fit(X_train, y_train)
-
     mae, mae_relative, r2 = xgb_model.evaluate(X_test, y_test)
     print(f"Mean Absolute Error: {mae}")
     print(f"Relative Mean Absolute Error: {mae_relative}")
     print(f"R^2 Score: {r2}")
 
-    best_params = tune_hyperparameters(X_train, y_train, X_test, y_test, TREE_METHOD) #Bayesian Optimization
-
-    #xgb_model.save_model(PATH_MODEL)
-    #print(f"XGBoost Model saved to {PATH_MODEL}")
+    xgb_model.save_model(PATH_MODEL)
+    print(f"XGBoost Model saved to {PATH_MODEL}")
     #X_train.to_csv(PATH_PRE_PROCESSED_TRAIN_DATA)
     #X_test.to_csv(PATH_PRE_PROCESSED_TEST_DATA)
 
