3af823950fc42a79a6a6c15910591a0e1b465ce2
saran <sarandeep1999@gmail.com>
2024-03-13 20:41:11 +0100

    add dynamic H3 processing

--- src/processing_utils/h3_preprocessing.py
+++ src/processing_utils/h3_preprocessing.py
@@ -6,15 +6,18 @@ import time
 import logging
 import folium
 from datetime import timedelta
+import numpy as np
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s')
+
 
 def update_progress(pbar):
     pbar.update(1)
     time.sleep(0.5)
 
-def get_h3_index(data, res, polygon = False):
-    '''
+
+def get_h3_index(data, res, polygon=False):
+    """
     Adds H3 index and Hexagonal cell geometry column.
 
     Parameters:
@@ -23,8 +26,8 @@ def get_h3_index(data, res, polygon = False):
 
     Returns:
     - final: The enhanced DataFrame with an added H3 index column and Hexagonal cell geometry column.
-    '''
-    if (polygon):
+    """
+    if polygon:
         total = 6
     else:
         total = 5
@@ -42,7 +45,7 @@ def get_h3_index(data, res, polygon = False):
         data = data[~(data.h3index == '0')]
         update_progress(pbar)
 
-        if(polygon):
+        if polygon:
             data = data.h3.h3_to_geo_boundary()
             update_progress(pbar)
 
@@ -52,6 +55,71 @@ def get_h3_index(data, res, polygon = False):
 
     logging.info('H3 Features Added')
     return data
+
+
+def linear_resolution(distance, min_distance, max_distance, high_res, default_res):
+    """
+    Linearly interpolate H3 resolution based on the distance.
+
+    Parameters:
+    - distance: The distance from the point to FRA.
+    - min_distance: The distance at which the highest resolution (high_res) is applied.
+    - max_distance: The distance at which the lowest resolution (default_res) is applied.
+    - high_res: The highest H3 resolution to use.
+    - default_res: The default H3 resolution to use.
+
+    Returns:
+    - An integer representing the calculated H3 resolution.
+    """
+    if distance <= min_distance:
+        return high_res
+    elif distance >= max_distance:
+        return default_res
+    else:
+        return int(round(np.interp(distance, [min_distance, max_distance], [high_res, default_res])))
+
+
+def get_dynamic_h3_index(data, default_res, high_res, min_distance, max_distance, polygon=False):
+    """
+    Adds H3 index and Hexagonal cell geometry column with smoothly changing resolution.
+
+    Additional Parameters:
+    - min_distance: Minimum distance for highest resolution.
+    - max_distance: Maximum distance for default resolution.
+    """
+    if polygon:
+        total = 6
+    else:
+        total = 5
+    logging.info("Starting dynamic H3 preprocessing...")
+    data = data.copy()
+
+    with tqdm(total=total, desc="Processing", dynamic_ncols=True) as pbar:
+        data.rename(columns={'latitude': 'lat', 'longitude': 'lng'}, inplace=True)
+        update_progress(pbar)
+
+        # Calculate dynamic resolution based on distance
+        data['dynamic_res'] = data['distance'].apply(
+            lambda x: linear_resolution(x, min_distance, max_distance, high_res, default_res))
+
+        data['h3index'] = data.apply(lambda row: h3.geo_to_h3(row['lat'], row['lng'], row['dynamic_res']), axis=1)
+        update_progress(pbar)
+
+        data['timestamp'] = pd.to_datetime(data['timestamp'])
+        data = data[~(data['h3index'] == '0')]
+        update_progress(pbar)
+
+        if polygon:
+            data['geometry'] = data['h3index'].apply(lambda x: h3.h3_to_geo_boundary(x, geo_json=True))
+            update_progress(pbar)
+
+        data = data.rename(columns={'lat': 'latitude', 'lng': 'longitude'})
+        data.reset_index(inplace=True, drop=True)
+        update_progress(pbar)
+
+    logging.info('Dynamic H3 features added')
+    return data
+
 
 def plot_h3(df, save_html=False, file_name="map.html"):
     """
@@ -74,8 +142,9 @@ def plot_h3(df, save_html=False, file_name="map.html"):
 
     return m
 
+
 def add_density(data):
-    '''
+    """
     This function adds density metrics to the given DataFrame based on the number of unique aircraft
     in the same h3index within the past 10, 30, and 60 minutes.
 
@@ -84,8 +153,8 @@ def add_density(data):
 
     Returns:
     - The DataFrame with added density columns for the past 10, 30, and 60 minutes.
-    '''
-    
+    """
+
     data['timestamp'] = pd.to_datetime(data['timestamp'])
     df = data.copy()
     df = df.sort_values('timestamp').reset_index(drop=True)
@@ -128,6 +197,7 @@ def add_density(data):
     data['density_60_minutes_past'] = density_60_minutes_past
     return data
 
+
 def weekday_column(traindata):
     weekday_df = traindata.filter(regex='^weekday_')
     traindata['weekday'] = weekday_df.idxmax(axis=1)
@@ -151,6 +221,7 @@ def test_data_h3_average(traindata, testdata):
         (['h3index', 'hour'], 'average_hourly_altitude', 'average_hourly_avg_altitude'),
     ]:
         testdata = calculate_average_and_merge(traindata, testdata, group_cols, col, new_col_name)
-    testdata.rename(columns={'average_hourly_hexbin_density': 'hexbin_hourly_density', 'average_hourly_avg_speed': 'average_hourly_speed', 'average_hourly_avg_altitude':'average_hourly_altitude'}, inplace=True)
+    testdata.rename(columns={'average_hourly_hexbin_density': 'hexbin_hourly_density',
+                             'average_hourly_avg_speed': 'average_hourly_speed',
+                             'average_hourly_avg_altitude': 'average_hourly_altitude'}, inplace=True)
     return testdata
-
